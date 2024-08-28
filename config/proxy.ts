11d535d25fc6628a6ea7beaa1b7c580decbe6bae
IronMan <zym2894209453@163.com>
2024-08-28 14:16:27 +0800

    bugfix

--- config/proxy.ts
+++ config/proxy.ts
@@ -15,7 +15,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'http://127.0.0.1:8080',
+      target: 'http://192.168.0.51:52001',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
