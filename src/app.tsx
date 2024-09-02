51f257700d22f3d4edcd62109131abc1453c2ee3
IronMan <zym2894209453@163.com>
2024-09-02 10:35:59 +0800

    bugfix

--- src/app.tsx
+++ src/app.tsx
@@ -168,6 +168,6 @@ export const layout: RunTimeLayoutConfig = ({initialState, setInitialState}) => 
  * @doc https://umijs.org/docs/max/request#配置
  */
 export const request: RequestConfig = {
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: '/api',
   ...errorConfig,
 }
