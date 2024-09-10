7cd791dbead65b10a58bbf54eebd30e551d4e968
IronMan <zym2894209453@163.com>
2024-09-10 11:10:16 +0800

    bugfix

--- src/components/SearchSelect/index.tsx
+++ src/components/SearchSelect/index.tsx
@@ -3,24 +3,46 @@ import {useEffect, useState} from "react";
 
 export default (
   {
+    defaultValue = undefined,
+    parent = undefined,
+    repeat = false,
+    readOnly = false,
+    hiddenData = [],
     request = async () => [],
-    onChange = (value) => {
+    onChange = () => {
     },
   }: {
-    request: () => Promise<never[]>,
-    onChange: (value: any) => void,
-    getValue?: () => any
+    parent?: any, //父节点
+    defaultValue?: any, //默认值
+    hiddenData?: any[], //默认值
+    request: (value: any) => Promise<never[]>, //请求方法
+    onChange: (value: any, option: any) => void, //选中回调
+    getValue?: () => any, //获取值
+    repeat?: boolean, //不允许重复
+    readOnly?: boolean, // 只读
   }
 ): React.ReactElement => {
   const [options, setOptions] = useState<any[]>([]);
+  const [showOptions, setShowOptions] = useState<any[]>(options);
+  const [selected, setSelected] = useState<string>('');
+  const [pa, setParent] = useState<any>(parent);
 
   useEffect(() => {
-    request().then(res => {
+    request(pa).then(res => {
       setOptions(res)
+      setShowOptions(res)
     })
   }, []);
 
-  return (
-    <Select options={options} onChange={onChange}></Select>
-  )
+  useEffect(() => {
+    setShowOptions(repeat ? [...options.filter((o) => o.value !== selected).filter((o) => !hiddenData.includes(o.value))] : options);
+  }, [selected, options])
+
+  return <>
+    {readOnly ? (options.filter(item => item.value === defaultValue).length > 0 ? options.filter(item => item.value === defaultValue)[0].label : defaultValue) :
+      <Select options={showOptions} onChange={(value, option) => {
+        setSelected(value);
+        onChange(value, option)
+      }} defaultValue={defaultValue} style={{width: '100%'}}></Select>}
+  </>
 }
